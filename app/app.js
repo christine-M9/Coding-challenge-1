b1b444fbe0601eae572bcba8fa8dcb280231c668
Christine <christinekhekama99@gmail.com>
2024-06-02 21:27:38 +0300

    storing weight and height data in variable

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Question1 :storing both Mark's and John's weight and height in a variable 
+let markWeight1 = 78;
+let markHeight1 = 1.69;
+let johnWeight1 = 92;
+let johnHeight1 = 1.95;
+
+let markWeight2 = 95;
+let markHeight2 = 1.88;
+let johnWeight2 = 85;
+let johnHeight2 = 1.76;
+
+// Question2: 
